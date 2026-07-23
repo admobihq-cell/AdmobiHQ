4b03435b3692e597086ac360aef76a09d3602b6b
Victor Musembi <166705594+VictorNzai@users.noreply.github.com>
2026-07-23 04:19:03 +0300

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  serverExternalPackages: ['sharp'],
   outputFileTracingExcludes: {
     '*': [
       'node_modules/@prisma/engines/**',
